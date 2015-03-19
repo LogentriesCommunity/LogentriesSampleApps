7e43f89915e9e529561433e1948026bd8cb178be
stephen <stephen@logentries.com>
2015-03-19 11:34:41 +0000

    Add sample .NET projects
    Add sample .NET projects for nlog and log4net

---
+++ LogentriesLog4NetSampleApp/LogentriesLog4NetSampleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+namespace LogentriesLog4NetSampleApp
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger( typeof(Program));
+        static void Main(string[] args)
+        {
+            logger.Debug("Debugging message");
+            logger.Info("Info message");
+            logger.Warn("Warning message");
+            logger.Error("Error message");
+            logger.Fatal("Fatal message");
+
+            String x = System.Console.ReadLine();
+        }
+    }
+}
---
+++ LogentriesNlogSampleApp/LogentriesNlogSampleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+namespace LogentriesNlogSampleApp
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Debug("Debugging message");
+            logger.Info("Info message");
+            logger.Warn("Warning message");
+            logger.Error("Error message");
+            logger.Fatal("Fatal message");
+            String a = System.Console.ReadLine();
+        }
+    }
+}
