44cd06f3000366c93876f2fde02c92a6e0898602
Kieran O'Brien <kobrien@rapid7.com>
2016-05-26 15:17:31 +0100

    Add files via upload

---
+++ LogentriesLog4NetSampleApp copy/LogentriesLog4NetSampleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+namespace LogentriesLog4NetSampleApp
+{
+    class Program
+    {
+        private static readonly ILog loggerLe = LogManager.GetLogger( typeof(Program));
+        
+
+        static void Main(string[] args)
+        {
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+            loggerLe.Info("This is a test message");
+
+            String x = System.Console.ReadLine();
+        }
+    }
+}
